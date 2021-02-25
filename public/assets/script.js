795cf579dc3f4405f3b0b0a8e29df08df23fd133
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-25 10:22:51 -0800

    broken delete button is WIP. fixed font awesome icon call, hoping this was the answer.

--- public/assets/script.js
+++ public/assets/script.js
@@ -113,7 +113,7 @@ const renderNoteList = (notes) => {
 
         if(withDeleteButton) {
             const $delBtn = $(
-                "<i class='fas-fa-trash-alt float-right text-danger delete-note'>"
+                "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
                 );
             $li.append($delBtn);
         }
