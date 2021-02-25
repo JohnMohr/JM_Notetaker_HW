e4d152e947b8462bad4ddfb8fc0e20bc3bb1ade4
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-25 09:06:13 -0800

    split index into note and index html

--- server.js
+++ server.js
@@ -2,49 +2,68 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+//init server
 const app = express();
 
+// testing for port and heroku
 const PORT = process.env.PORT || 3030;
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static( "public" ));
 
-app.get("./notes", (req,res) =>{
+
+app.get("/notes", (req, res) =>{
+    res.sendFile(path.join( __dirname, "./public/notes.html"));
+})
+
+
+//API ROUTES
+
+//show json for notes
+app.get("/api/notes", (req,res) =>{
 
     //get db.json
     fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", (err, data) => {
         if(err){
             throw(err)
         }
+        // send okay status. parse file contents as json.
         res.status(200).json(JSON.parse(data))
     })
 })
 
+
+// add note route
 app.post("/api/notes", (req, res) => {
     fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8",(err, file) => {
         if(err){
             throw(err)
         }
-
+        // parse file as json
         const data = JSON.parse(file)
         const note = {...req.body};
 
+        //create note object from req.body
         note.id = parseInt(data[data.length-1].id);
         note.id ++;
 
+        //add note to data
         data.push(note);
 
+        //write new data to file
         fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(data, null, 4), err => {
             if(err){
                 throw(err)
             }
-
+            // send okay. send data.
             res.status(200).json(data);
         })
     })
 })
 
+//delete note route
 app.delete("/api/notes/:id", (req, res)=>{
 
     // read db.json
@@ -77,6 +96,7 @@ app.delete("/api/notes/:id", (req, res)=>{
     })
 })
 
+// default route
 app.get("*", (req, res) => {
     res.sendFile(path.join( __dirname, "./public.index.html"));
 })
